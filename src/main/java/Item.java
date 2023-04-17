5c1c4f8668eda4b8c71da0c0a6c039b7044976b9
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-04-17 12:23:28 -0400

    finished my tests and item class... i think.......

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -10,6 +10,30 @@ public class Item {
     }
 
     public void setName(String name){
-        this.name = name; 
+        this.name = name;
+    }
+
+    public String getPrice(){
+        return price;
+    }
+
+    public void setPrice(String price){
+        this.price = price;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String drink){
+        this.type = type;
+    }
+
+    public void setExpiration(String expiration){
+        this.expiration = expiration;
+    }
+
+    public String getExpiration() {
+        return expiration;
     }
 }
--- src/main/java/TestItems.java
+++ src/main/java/TestItems.java
@@ -10,4 +10,60 @@ public class TestItems {
         String actual = item.getName();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void setName() throws Exception{
+        String expected = "Jennifer";
+        item.setName("Jennifer");
+        String actual = item.getName();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPrice() throws Exception{
+        String expected = "5.50";
+        String actual = item.getPrice();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPrice() throws Exception{
+        String expected = "7.85";
+        item.setPrice("7.85");
+        String actual = item.getPrice();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getType() throws Exception{
+        String expected = "Food";
+        String actual = item.getType();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setType() throws Exception{
+        String expected = "Drink";
+        item.setType("Drink");
+        String actual = item.getType();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExpiration() throws Exception{
+        String expected = "01/05/1995";
+        String actual = item.getExpiration();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setExpiration() throws Exception{
+        String expected = "02/06/1996";
+        item.setExpiration("01/06/1996");
+        String actual = item.getExpiration();
+        assertEquals(expected, actual);
+    }
+
+
+
 }
